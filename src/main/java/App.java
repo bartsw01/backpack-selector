0cbb8976583c071c9c0b0f0ee9817b6099c46de8
Bart Webber and David Cuthell <bart@>
2017-08-09 16:17:23 -0700

    add drop down menu to index to choose backpack type

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 /**
  * Created by Guest on 8/9/17.
@@ -18,5 +19,15 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        post("/backpacks/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String bpselection = request.queryParams("beSelection");
+            System.out.println(bpselection);
+            Backpack newBackpack = new Backpack(bpselection);
+            model.put("backpack", newBackpack);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
---
+++ src/test/java/models/BackpackTest.java
@@ -0,0 +1,11 @@
+package models;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 8/9/17.
+ */
+public class BackpackTest {
+
+
+}
